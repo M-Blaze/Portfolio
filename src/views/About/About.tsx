2a6cd249f9e5500b7dbe264c292b28e67ba01f97
Moulik Rai <raimoulik@gmail.com>
2022-10-02 18:37:49 +0545

    Edit texts in about and project pages

--- src/views/About/About.tsx
+++ src/views/About/About.tsx
@@ -3,9 +3,12 @@ import classNames from 'classnames'
 import Image from 'next/image'
 
 import styles from './About.module.scss'
+import { getDateDifferenceInYear } from '../../../helpers/date.helpers'
 
+const myBirthDate = '1997-07-26'
 const About = () => {
   const profileMask = classNames('image-mask', styles.mask1)
+  const myAge = getDateDifferenceInYear(new Date(), myBirthDate)
 
   return (
     <section id="about-me">
@@ -13,7 +16,7 @@ const About = () => {
         <Flex alignItems="center">
           <Box className="text-wrap" flex="1" padding="0 15px">
             <Heading as="h2">Hey, This is Moulik!</Heading>
-            <strong>A professional fullstack developer</strong>
+            <strong className="pl-2">A professional fullstack developer</strong>
           </Box>
           <Box w="200px" textAlign="center">
             <div className={styles.imgHolder}>
@@ -81,17 +84,24 @@ const About = () => {
             <div className="list-holder capitalize">
               <ul className="info-list">
                 <li className="mb-1">
-                  <strong>Age:</strong> <span className="text-gray-500">25</span>
+                  <strong>Age:</strong>&nbsp;<span className="text-gray-500">{myAge}</span>
                 </li>
                 <li className="mb-1">
-                  <strong>Interests:</strong>&nbsp;
-                  <span className="text-gray-500">
-                    Programming, Competitive gaming, Competetive programming, Singing, Playing Guitar, Chess
-                  </span>
+                  <strong>Hobbies:</strong>
+                  <div className="list-holder">
+                    <ul className="hobbies-list text-gray-500 pl-2">
+                      <li>Guitar</li>
+                      <li>Gaming</li>
+                      <li>Chess</li>
+                    </ul>
+                  </div>
                 </li>
                 <li>
-                  <strong>Education:</strong>&nbsp;
-                  <span className="text-cyan-500">B.E. Information Technology</span>
+                  <strong>Education:</strong>
+                  <br />
+                  <div className="text-wrap pl-2">
+                    <span className="text-cyan-500">B.E. Information Technology</span>
+                  </div>
                 </li>
               </ul>
             </div>
--- src/views/Projects/Projects.tsx
+++ src/views/Projects/Projects.tsx
@@ -26,14 +26,16 @@ const Projects = () => {
             </Flex>
             <Box className="content-wrap" flex="1" paddingTop="12px">
               <p>
-                Envynote is a simple <span className="text-red-500">Progressive Web App</span> used to create notebooks
-                and write down notes inside it. Envynote is built using&nbsp;
-                <span className="text-red-500">React, Redux, Firebase</span>.
+                Envynote is a simple Progressive Web App used to create notebooks and write down notes inside it.
+                Envynote uses React/Redux for frontend and Firebase&apos;s cloud service for backend and database.
               </p>
-              <p>
-                For Authentication, Users can either create an account or login via{' '}
-                <span className="text-red-500">Google Authentication Api</span>.
+              <p style={{ marginBottom: '15px' }}>
+                For Authentication, Users can either create an account or login via Google Authentication Api.
               </p>
+              <Flex>
+                <Box marginRight="10px">Tech stack:</Box>
+                <Box flex="1">React, Redux, Firebase, Google Auth Api</Box>
+              </Flex>
             </Box>
           </Box>
           <Box borderRadius="5" backgroundColor="gray.200" padding="15px">
@@ -55,25 +57,14 @@ const Projects = () => {
                 As the lead developer, I had the whole responsibility to design and develop the architecture of Titan.
               </p>
               <p style={{ marginBottom: '15px' }}>
-                Titan uses <span className="text-red-500">Quasar framework of Vue.js</span> for the frontend,&nbsp;
-                <span className="text-red-500">Express.js</span> for the backend and&nbsp;
-                <span className="text-red-500">MongoDB</span> for the Database. A child process of&nbsp;
-                <span className="text-red-500">Python/pandas</span> is spawned for the{' '}
-                <span className="text-red-500">Data Analysis</span> which analyzes the customer&apos;s data and uploads
-                the analyzed data in the database.&nbsp;
-                <span className="text-red-500">D3.js</span> is used for the visualization of the analyzed data.
+                Titan uses Quasar framework of Vue.js for the frontend, Express.js for the backend and MongoDB for the
+                Database. A child process of Python/pandas is spawned for the Data Analysis which analyzes the
+                customer&apos;s data and uploads the analyzed data in the database. D3.js is used for the visualization
+                of the analyzed data.
               </p>
               <Flex>
                 <Box marginRight="10px">Tech stack:</Box>
-                <Box flex="1">
-                  <span className="text-red-500">Quasar (Vue)</span>, <span className="text-red-500">Express.Js</span>
-                  ,&nbsp;
-                  <span className="text-red-500">MongoDB</span>,&nbsp;
-                  <span className="text-red-500">D3.js</span>,&nbsp;
-                  <span className="text-red-500">Pandas (python)</span>,&nbsp;
-                  <span className="text-red-500">Sendgrid</span>,&nbsp;
-                  <span className="text-red-500">Puppeeter</span>
-                </Box>
+                <Box flex="1">Quasar (Vue), Vuex, Express.js, MongoDB, D3.js, Pandas (python), Sendgrid, Puppeeter</Box>
               </Flex>
             </Box>
           </Box>
