f2d45f86b0cc7cb8220a929e129af6dac2fe2ee0
Moulik Rai <raimoulik@gmail.com>
2022-10-05 12:45:11 +0545

    Add date helper

--- helpers/date.helpers.ts
+++ helpers/date.helpers.ts
@@ -5,3 +5,15 @@ type InputType = string | Date | number
 export const getDateDifferenceInYear = (to: InputType, from: InputType) => {
   return dayjs(to).diff(from, 'year')
 }
+
+export const getDateDifferenceForWorkTime = (to: InputType, from: InputType) => {
+  const endDate = to === 'present' ? new Date() : to
+  const totalMonths = dayjs(endDate).diff(from, 'month') + 1
+  const years = Math.floor(totalMonths / 12)
+  const months = Math.floor(totalMonths % 12)
+  const formattedStartDate = dayjs(from).format('MMM YYYY')
+  const formattedEndDate = to === 'present' ? 'Present' : dayjs(to).format('MMM YYYY')
+  const stringEnd = years ? `${years} yrs ${months} mos` : `${months} mos`
+
+  return `${formattedStartDate} - ${formattedEndDate} : ${stringEnd}`
+}
