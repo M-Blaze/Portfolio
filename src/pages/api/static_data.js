cc55546b1d2e4dca1bad69cf8e875c0e525d5545
Moulik Rai <raimoulik@gmail.com>
2022-10-17 10:40:58 +0545

    chore: add likes property

--- src/pages/api/static_data.js
+++ src/pages/api/static_data.js
@@ -2,52 +2,62 @@ export const blogData = [
   {
     id: 1,
     title: 'Ultrasonography of Left Ovary using Other Contrast',
+    likes: 1,
     createdAt: '2021-09-15'
   },
   {
     id: 2,
     title: 'Removal of Synthetic Substitute from Left Knee Joint, Percutaneous Endoscopic Approach',
+    likes: 5,
     createdAt: '2021-10-22'
   },
   {
     id: 3,
     title:
       'Dilation of Left Internal Iliac Artery, Bifurcation, with Four or More Drug-eluting Intraluminal Devices, Open Approach',
+    likes: 12,
     createdAt: '2021-12-06'
   },
   {
     id: 4,
     title: 'Insertion of Infusion Device into Ureter, Percutaneous Endoscopic Approach',
+    likes: 7,
     createdAt: '2021-12-18'
   },
   {
     id: 5,
     title: 'Division of Right Hip Tendon, Open Approach',
+    likes: 6,
     createdAt: '2021-11-16'
   },
   {
     id: 6,
     title: 'Insertion of Infusion Device into Right Shoulder Joint, Open Approach',
+    likes: 3,
     createdAt: '2021-12-16'
   },
   {
     id: 7,
     title: 'Removal of Drainage Device from Right Eye, Open Approach',
+    likes: 8,
     createdAt: '2021-09-28'
   },
   {
     id: 8,
     title: 'Replacement of Right Femoral Shaft with Nonautologous Tissue Substitute, Open Approach',
+    likes: 9,
     createdAt: '2021-09-02'
   },
   {
     id: 9,
     title: 'Drainage of Right Lower Leg Subcutaneous Tissue and Fascia, Percutaneous Approach',
+    likes: 167,
     createdAt: '2022-03-21'
   },
   {
     id: 10,
     title: 'Drainage of Left Foot, Percutaneous Approach, Diagnostic',
+    likes: 18,
     createdAt: '2021-07-29'
   }
 ]
